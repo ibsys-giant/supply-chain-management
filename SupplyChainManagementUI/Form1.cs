d26b8faa3c3bb7f991f25742d52ad7c79e57601f
Michael Kunzmann <kumi1013@hs-karlsruhe.de>
2015-06-21 18:55:54 +0200

    Added UI project

--- SupplyChainManagementUI/Form1.cs
+++ SupplyChainManagementUI/Form1.cs
@@ -16,5 +16,10 @@ namespace SupplyChainManagementUI
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
     }
 }
